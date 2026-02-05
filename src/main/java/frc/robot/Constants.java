23c82c8cf5da84d1dcbf3a67aa0e0e5978e652bd
mcr11131 <11131@mcrobotics.org>
2026-02-05 15:57:16 -0500

    Update Constants.java
    Making it shoot the correct way.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -37,8 +37,8 @@ public final class Constants {
 
   public static final class FuelConstants {
     // Motor controller IDs for Fuel Mechanism motors
-    public static final int FEEDER_MOTOR_ID = 15;
-    public static final int INTAKE_LAUNCHER_MOTOR_ID = 17;
+    public static final int FEEDER_MOTOR_ID = 17;
+    public static final int INTAKE_LAUNCHER_MOTOR_ID = 15;
 
     // Current limit and nominal voltage for fuel mechanism motors.
     public static final int FEEDER_MOTOR_CURRENT_LIMIT = 60;
@@ -49,8 +49,8 @@ public final class Constants {
     // See the Software Guide for tuning information
     public static final double INTAKING_FEEDER_VOLTAGE = -12;
     public static final double INTAKING_INTAKE_VOLTAGE = 10;
-    public static final double LAUNCHING_FEEDER_VOLTAGE = 9;
-    public static final double LAUNCHING_LAUNCHER_VOLTAGE = 10.6;
+    public static final double LAUNCHING_FEEDER_VOLTAGE = -9;
+    public static final double LAUNCHING_LAUNCHER_VOLTAGE = -10.6;
     public static final double SPIN_UP_FEEDER_VOLTAGE = -6;
     public static final double SPIN_UP_SECONDS = 1;
   }
