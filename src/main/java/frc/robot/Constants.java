db22faa162358042c76266962d0816b957b76232
mcr11131 <11131@mcrobotics.org>
2026-02-10 16:13:05 -0500

    Adjusted voltage values to spin the right way
    maybe not for eject

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -48,14 +48,14 @@ public final class Constants {
     // based on exact robot construction.
     // See the Software Guide for tuning information
     public static final double INTAKING_FEEDER_VOLTAGE = 12;
-    public static final double INTAKING_INTAKE_VOLTAGE = 10;
+    public static final double INTAKING_INTAKE_VOLTAGE = -10;
     public static final double LAUNCHING_FEEDER_VOLTAGE = 9;
     public static final double LAUNCHING_LAUNCHER_VOLTAGE = 10.6;
     public static final double SPIN_UP_FEEDER_VOLTAGE = -6;
     public static final double SPIN_UP_SECONDS = 1;
     //New code for ejecting voltage levels. Negative of intaking.
     public static final double EJECT_FEEDER_VOLTAGE = 12;
-    public static final double EJECT_LAUNCHER_VOLTAGE = -10;
+    public static final double EJECT_LAUNCHER_VOLTAGE = 10;
   }
 
   public static final class SimConstants {
--- src/main/java/frc/robot/commands/Intake.java
+++ src/main/java/frc/robot/commands/Intake.java
@@ -25,7 +25,7 @@ public class Intake extends Command {
   @Override
   public void initialize() {
     fuelSubsystem
-        .setIntakeLauncherRoller(SmartDashboard.getNumber("Intaking intake roller value", INTAKING_INTAKE_VOLTAGE));
+        .setIntakeLauncherRoller(SmartDashboard.getNumber("Intaking intake roller value",INTAKING_INTAKE_VOLTAGE));
     fuelSubsystem.setFeederRoller(SmartDashboard.getNumber("Intaking feeder roller value", INTAKING_FEEDER_VOLTAGE));
   }
 
