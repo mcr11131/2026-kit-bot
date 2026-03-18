10631a3115731f88d2d4254ed1997878ab8cbe07
mcr11131 <11131@mcrobotics.org>
2026-03-18 17:27:13 -0400

    New auto
    Blocks the trench

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,7 +20,7 @@ import frc.robot.commands.Intake;
 import frc.robot.commands.LaunchSequence;
 import frc.robot.commands.AlignToScore;
 import frc.robot.commands.Spin180;
-
+import frc.robot.commands.TrenchAuto;
 import frc.robot.subsystems.CANClimbSubsystem;
 import frc.robot.subsystems.CANDriveSubsystem;
 import frc.robot.subsystems.CANFuelSubsystem;
@@ -62,7 +62,9 @@ public class RobotContainer {
     // Set the options to show up in the Dashboard for selecting auto modes. If you
     // add additional auto modes you can add additional lines here with
     // autoChooser.addOption
-    autoChooser.setDefaultOption("Autonomous", new ExampleAuto(driveSubsystem, fuelSubsystem));
+    autoChooser.setDefaultOption("Fadeaway", new ExampleAuto(driveSubsystem, fuelSubsystem));
+    autoChooser.addOption("Trench Blocker", new TrenchAuto(driveSubsystem));
+
   }
 
   /**
