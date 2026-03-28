6a3b49943d20df1ef943f272cc8bca11f3685005
mcr11131 <11131@mcrobotics.org>
2026-03-28 10:16:43 -0400

    Shorter Auto Distance

--- src/main/java/frc/robot/commands/ExampleAuto.java
+++ src/main/java/frc/robot/commands/ExampleAuto.java
@@ -22,7 +22,7 @@ public class ExampleAuto extends SequentialCommandGroup {
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
     // Drive forward, away from the hub for 1.5 meters using encoder-based distance control
-    new AutoDrive(driveSubsystem, 0.5, 0, 1.5),
+    new AutoDrive(driveSubsystem, 0.5, 0, 1.25),
     // Spin up the launcher for 1 second and then launch balls for 9 seconds, for a
     // total of 10 seconds
     new SpinUp(ballSubsystem).withTimeout(SPIN_UP_SECONDS),
